49688128a5570f04f73c8994b132331a311b1117
chocoisntlate <130369559+chocoisntlate@users.noreply.github.com>
2025-06-05 23:40:47 +0700

    some styling and template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './styles.css'
+
 import item from './item.js'
 import project from './project.js'
 import {displayProject} from './displayProject.js'
+
 
 let project1 = new project("project1")
 let item1 = new item("finish the project", "do it now", new Date(2025, 5, 20), "high")
