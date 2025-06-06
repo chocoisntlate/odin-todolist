1bc4f4999139fce510995e9bb13f56069b7b3750
chocoisntlate <130369559+chocoisntlate@users.noreply.github.com>
2025-06-06 14:56:34 +0700

    styling progress

--- src/displayProject.js
+++ src/displayProject.js
@@ -1,27 +1,17 @@
 import { format } from 'date-fns'
 
 function displayProject(project) {
+    let projectContent = document.getElementById("project-content")
     for (let item of project.items) {
         let itemContainer = document.createElement('div')
+        itemContainer.setAttribute("class", "item-container")
 
-        let title = document.createElement('div')
+        let title = document.createElement('span')
         title.textContent = item.title
 
-        let description = document.createElement('div')
-        description.textContent = item.description
-
-        let due = document.createElement('div')
-        due.textContent = format(item.due, 'dd-MM-yyyy')
-
-        let priority = document.createElement('div')
-        priority.textContent = item.priority
-
         itemContainer.appendChild(title)
-        itemContainer.appendChild(description)
-        itemContainer.appendChild(due)
-        itemContainer.appendChild(priority)
 
-        document.body.appendChild(itemContainer)
+        projectContent.appendChild(itemContainer)
     }
 }
 
